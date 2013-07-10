861533efa02fe14c62bd8f5da1e6dfe91d9d70f3
sarah bland <sarahjbland@yahoo.com>
2013-07-10 00:25:42 +0000

    modify web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+buf = readFileSync('index.html');
+var bugger = new Buffer(buf);
+var output = bugger.toString();
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');//
+response.send(output);
 });
 
 var port = process.env.PORT || 5000;

adac93415b85687d3f8539040965f27ec3676355
sarah bland <sarahjbland@yahoo.com>
2013-07-10 00:36:16 +0000

    modify web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-buf = readFileSync('index.html');
+var buf = fs.readFileSync('index.html');
 var bugger = new Buffer(buf);
 var output = bugger.toString();
 
